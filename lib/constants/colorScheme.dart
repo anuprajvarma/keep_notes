3176b4163ec3172e1a6656939651df20f7e6dd44
anupraj varma <anupraj1854@gmail.com>
2021-09-21 15:02:21 +0530

    created keep notes app

---
+++ lib/constants/colorScheme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorScheme{
+    static Color dark= Color(0xff54436B);
+    static Color green3= Color(0xff50CB93);
+    static Color green2= Color(0xff71EFA3);
+    static Color green1= Color(0xffACFFAD);
+}
